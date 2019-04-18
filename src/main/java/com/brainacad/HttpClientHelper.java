ca2d5256fda50aebb8113ab163402c4a22c28f47
ig <po4talen@gmail.com>
2019-04-18 21:45:50 +0300

    Back-end - udpate 1

--- src/main/java/com/brainacad/HttpClientHelper.java
+++ src/main/java/com/brainacad/HttpClientHelper.java
@@ -1,5 +1,6 @@
 package com.brainacad;
 
+import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -10,15 +11,19 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.security.PublicKey;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.apache.http.protocol.HTTP.USER_AGENT;
 
 public class HttpClientHelper {
 
-    public static HttpResponse get(String endpointUrl, String parameters){
-       //TODO: написать метод для GET запроса с хедерами по умолчанию
-       return null;
+    public static HttpResponse get(String endpointUrl, String parameters) throws IOException {
+        //TODO: написать метод для GET запроса с хедерами по умолчанию
+        Map<String, String> headers=new HashMap<>();
+        headers.put("User-Agent", "My-Test-User-Agent");
+        return get(endpointUrl, parameters,headers);
     }
 
     //REST GET запрос
@@ -41,9 +46,11 @@ public class HttpClientHelper {
     }
 
 
-    public static HttpResponse post(String endpointUrl, String parameters){
+    public static HttpResponse post(String endpointUrl, String parameters) throws IOException {
         //TODO: написать метод для POST запроса с хедерами по умолчанию
-        return null;
+        Map<String,String> headers = new HashMap<>();
+        headers.put("User-Agent", "My-Test-User-Agent");
+        return post(endpointUrl, parameters,headers);
     }
 
     public static HttpResponse post(String endpointUrl, String body, Map<String, String> headers) throws IOException{
@@ -84,4 +91,8 @@ public class HttpClientHelper {
     }
 
     //TODO: допишите методы для запросов PUT, PATCH и DELETE
+    public static HttpResponse post(){
+
+        return null;
+    }
 }
--- src/test/java/com/brainacad/RestTest.java
+++ src/test/java/com/brainacad/RestTest.java
@@ -16,15 +16,9 @@ public class RestTest{
     public void checkGetResponseStatusCode() throws IOException {
         String endpoint="/api/users";
 
-        //TODO: Избавится он хедеров в тесте добавив методы с хедерами по умолчанию в класс HttpClientHelper
-        //Создаём переменую headers типа Map
-        Map<String, String> headers=new HashMap<>();
-        //Добавляем в headers наш заголовок
-        headers.put("User-Agent", "My-Test-User-Agent");
-
         //Выполняем REST GET запрос с нашими параметрами
         // и сохраняем результат в переменную response.
-        HttpResponse response = HttpClientHelper.get(URL+endpoint,"page=2", headers);
+        HttpResponse response = HttpClientHelper.get(URL+endpoint,"page=2");
 
         //получаем статус код из ответа
         int statusCode = response.getStatusLine().getStatusCode();
@@ -36,15 +30,9 @@ public class RestTest{
     public void checkGetResponseBodyNotNull() throws IOException {
         String endpoint="/api/users";
 
-        //TODO: Избавится он хедеров в тесте добавив методы с хедерами по умолчанию в класс HttpClientHelper
-        //Создаём переменую headers типа Map
-        Map<String, String> headers=new HashMap<>();
-        //Добавляем в headers наш заголовок
-        headers.put("User-Agent", "My-Test-User-Agent");
-
         //Выполняем REST GET запрос с нашими параметрами
         // и сохраняем результат в переменную response.
-        HttpResponse response = HttpClientHelper.get(URL+endpoint,"page=2", headers);
+        HttpResponse response = HttpClientHelper.get(URL+endpoint,"page=2");
 
         //Конвертируем входящий поток тела ответа в строку
         String body=HttpClientHelper.getBodyFromResponse(response);
@@ -56,18 +44,12 @@ public class RestTest{
     public void checkPostResponseStatusCode() throws IOException {
         String endpoint="/api/users";
 
-        //TODO: Избавится он хедеров в тесте добавив методы с хедерами по умолчанию в класс HttpClientHelper
-        //Создаём переменую headers типа Map
-        Map<String, String> headers=new HashMap<>();
-        //Добавляем в headers наш заголовок
-        headers.put("User-Agent", "My-Test-User-Agent");
-
         //создаём тело запроса
         String requestBody="{\"name\": \"morpheus\",\"job\": \"leader\"}";
 
         //Выполняем REST POST запрос с нашими параметрами
         // и сохраняем результат в переменную response.
-        HttpResponse response = HttpClientHelper.post(URL+endpoint,requestBody, headers);
+        HttpResponse response = HttpClientHelper.post(URL+endpoint,requestBody);
 
         //получаем статус код из ответа
         int statusCode = response.getStatusLine().getStatusCode();
